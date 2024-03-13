57a000364abfd1a2911718c8fdebc395d22bcb5e
BrownOdyssey <83645833+chldudqlsdl@users.noreply.github.com>
2024-03-14 00:26:22 +0900

    240313 fin

--- 240313/code/4-화면이동과데이터전달/BMI/BMI/ResultViewController.swift
+++ 240313/code/4-화면이동과데이터전달/BMI/BMI/ResultViewController.swift
@@ -17,7 +17,7 @@ class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        BMILabel.text = String(format: "%.2f", BMI!)
+        BMILabel.text = String(format: "%.1f", BMI!)
         configureUI()
     }
     
@@ -29,7 +29,8 @@ class ResultViewController: UIViewController {
         
         BMILabel.layer.cornerRadius = 10
         BMILabel.layer.masksToBounds = true
-        BMILabel.backgroundColor = .gray
+        BMILabel.backgroundColor = getBackgroundColor(bmi: String(format: "%.1f", BMI!))
+        adviceLabel.text = getBMIAdviceString(bmi: String(format: "%.1f", BMI!))
     }
     
     
@@ -37,5 +38,40 @@ class ResultViewController: UIViewController {
         dismiss(animated: true)
     }
     
+    func getBackgroundColor(bmi: String) -> UIColor {
+        let bmi = Double(bmi)!
+        switch bmi {
+        case ..<18.6:
+            return UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1)
+        case 18.6..<23.0:
+            return UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1)
+        case 23.0..<25.0:
+            return UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1)
+        case 25.0..<30.0:
+            return UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1)
+        case 30.0...:
+            return UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1)
+        default:
+            return UIColor.black
+        }
+    }
+    
+    func getBMIAdviceString(bmi: String) -> String {
+        let bmi = Double(bmi)!
+        switch bmi {
+        case ..<18.6:
+            return "저체중"
+        case 18.6..<23.0:
+            return "표준"
+        case 23.0..<25.0:
+            return "과체중"
+        case 25.0..<30.0:
+            return "중도비만"
+        case 30.0...:
+            return "고도비만"
+        default:
+            return ""
+        }
+    }
 
 }
